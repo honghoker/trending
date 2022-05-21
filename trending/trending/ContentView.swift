372738410b2a1e6821138ac3f40ebabffe947656
honghoker <ekflzhftk@naver.com>
2022-05-21 21:49:38 +0900

    Docs: Project Create

---
+++ trending/trending/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  trending
+//
+//  Created by 홍은표 on 2022/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ trending/trending/trendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  trendingApp.swift
+//  trending
+//
+//  Created by 홍은표 on 2022/05/21.
+//
+
+import SwiftUI
+
+@main
+struct trendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
